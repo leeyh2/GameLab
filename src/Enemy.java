c03d746e1ec2ac0fdfac32d6b18aa23992e850bf
burkegm <102096345+burkegm@users.noreply.github.com>
2022-04-20 19:51:55 -0400

    Update Enemy.java

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,62 +1,46 @@
-public class Player
+import java.util.Random;
+public class Enemy extends Player
 {
-    static String playerName;
-    protected int healthBar;
-    protected int silverCoins;
-    public static int healthPotions;
+    public  String enemyName;
+    public  int enemyHealthBar;
+    public  int enemyDmg;
 
-    
+    Random ranNum = new Random();
 
-
-
-    public Player(String playerName)
+    public Enemy(String playerName)
     {
-        this.playerName = playerName;   //player should only be able to set their name.
-        healthBar = 100;
-        silverCoins = 25; 
-        healthPotions= 3;              //silver coins need to be balanced player can't buy everything
+        super(playerName);
+        enemyName = "Kalel";
+        enemyHealthBar = 100;
+        silverCoins = 15;
+        enemyDmg = ranNum.nextInt(100);
 
     }
-   protected void addSilverCoins(int silverCoins)
-   {
-       //a method to add silver coins to player after defeating enemy
-       this.silverCoins += silverCoins;
-   }
-   protected void subtractSilverCoins(int silverCoins)
-   {
-       //a method to subtract silver coins to player after buying from store
-       this.silverCoins -= silverCoins;
-   }
 
-    public void setHealthBar(int healthBar) {
-        this.healthBar = healthBar;
+    public void setEnemyName(String enemyName) {
+        this.enemyName = enemyName;
     }
 
-    public void setPlayerName(String playerName) {
-        this.playerName = playerName;
+    public void setEnemyHealthBar(int enemyHealthBar) {
+        this.enemyHealthBar = enemyHealthBar;
     }
 
-    public void setSilverCoins(int silverCoins) {
-        this.silverCoins = silverCoins;
+    public int getEnemyHealthBar() {
+        return enemyHealthBar;
     }
 
-    public int getHealthBar() {
-        return healthBar;
+    public String getEnemyName() {
+        return enemyName;
     }
-
-    public int getSilverCoins() {
-        return silverCoins;
-    }
-
-    public String getPlayerName() {
-        return playerName;
-    }
-    public void displayDeck(Cards cards)
+    public int getEnemyDmg()
     {
-        System.out.println("-----"+cards.getCardName()+"-----");
-        System.out.println("Attack Damage: " + cards.getAttackDmg());
-        System.out.println("Block Stat: " + cards.getDmgReduced());
-        System.out.println("Health Stat: " + cards.getHealthRestore());
-
+        return enemyDmg;
+    }
+    public void displayEnemy()
+    {
+        System.out.println("-----Enemy-----");
+        System.out.println("Enemy Name: " + enemyName);
+        System.out.println("Enemy Health: " + healthBar);
+        System.out.println("---------------\n");
     }
 }
