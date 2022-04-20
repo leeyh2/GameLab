ca17487bd7900ddc54918cca425f8d9d2f2d5239
burkegm <102096345+burkegm@users.noreply.github.com>
2022-04-20 19:46:57 -0400

    Update Enemy.java

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,15 +1,20 @@
+import java.util.Random;
 public class Enemy extends Player
 {
-    protected String enemyName;
-    protected int enemyHealthBar;
+    public  String enemyName;
+    public static  int enemyHealthBar;
+    public  int enemyDmg;
 
+    Random ranNum = new Random();
 
     public Enemy(String playerName)
     {
         super(playerName);
         enemyName = "Kalel";
-        healthBar = 100;
+        enemyHealthBar = 100;
         silverCoins = 15;
+        enemyDmg = ranNum.nextInt(100);
+
     }
 
     public void setEnemyName(String enemyName) {
@@ -26,5 +31,16 @@ public class Enemy extends Player
 
     public String getEnemyName() {
         return enemyName;
+    }
+    public int getEnemyDmg()
+    {
+        return enemyDmg;
+    }
+    public void displayEnemy()
+    {
+        System.out.println("-----Enemy-----");
+        System.out.println("Enemy Name: " + enemyName);
+        System.out.println("Enemy Health: " + healthBar);
+        System.out.println("---------------\n");
     }
 }
